ded8a2035d3899e794933aed6bda6ab876337c36
Leonardo Costa <leonardocostapsi@gmail.com>
2023-05-14 11:43:41 -0300

    Adiciona @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 //Adiciona
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Leadcode site API')
+    .setDescription('Essa é a documentação da API do site da Leadcode.')
+    .setVersion('1.0')
+    .addTag('leadcode')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
