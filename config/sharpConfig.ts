2c5bfa8f6aba2b320443e716ed8aee2fcbb2f57b
Leonardo Costa <leonardocostapsi@gmail.com>
2023-06-17 09:22:46 -0300

    usa fit contain em resize do sharp

--- config/sharpConfig.ts
+++ config/sharpConfig.ts
@@ -20,6 +20,7 @@ export class SharpService {
       .resize({
         width: isCover ? 600 : 750,
         height: isCover ? 450 : 400,
+        fit: sharp.fit.contain,
       })
       .webp({ quality: 100 })
       .toFile(path.resolve(webpPath));
