4410dbeed9dc757773a19491b104bf3e54c839e4
Leonardo Costa <leonardocostapsi@gmail.com>
2025-03-30 08:50:58 -0300

    Remove controller heroImage

--- src/modules/storage/storage.module.ts
+++ src/modules/storage/storage.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
-import { StorageController } from 'src/modules/storage/heroImage/storage.controller';
 
 @Module({
-  controllers: [StorageController],
+  controllers: [],
   providers: [StorageService],
 })
 export class StorageModule {}
